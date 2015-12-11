538debc667df569edb0a345b1636d50e4d981ef0
Miguel <miguel.o.herrera@gmail.com>
2015-12-11 17:30:58 -0600

    Advance Building Blocks: Bubble Sort By

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,42 @@
 def bubble_sort(nums = [])
 
   while true
-    flag = false
+    unsorted = false
   
     for i in 1...nums.length
 
       if nums[i] < nums[i-1]
         nums[i], nums[i-1] = nums[i-1], nums[i]
-        flag = true 
+        unsorted = true 
       end
     end
   
-    break if flag == false
+    break if unsorted == false
   end
   "#{nums}"
 end
 
 puts bubble_sort([4,3,78,2,0,2])
 #=> [0,2,2,3,4,78]
+
+def bubble_sort_by(nums = [])
+
+  while true
+    unsorted = false
+      
+    for i in 1...nums.length
+
+      if (yield nums[i], nums[i-1]) == -1
+        nums[i], nums[i-1] = nums[i-1], nums[i]
+        unsorted = true
+      end
+
+    end
+      
+    break if unsorted == false
+  end
+  "#{nums}"
+end
+
+puts bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length <=> right.length }
+# => ["hi", "hey", "hello"]
